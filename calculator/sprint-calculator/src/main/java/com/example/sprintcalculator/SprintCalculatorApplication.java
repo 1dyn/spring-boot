8fc8c79661b13a5a337495c00b922c4075b203d9
Dongyeon Han <gksehddus202@naver.com>
2022-09-10 22:28:48 +0900

    Dollar-calculator practice

---
+++ calculator/sprint-calculator/src/main/java/com/example/sprintcalculator/SprintCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintCalculatorApplication.class, args);
+    }
+
+}
---
+++ calculator/sprint-calculator/src/test/java/com/example/sprintcalculator/SprintCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
